dad1585c2b1831c440b420ef7ad60b07061768c0
hasan jaser <hasan1999255@hotmail.com>
2021-01-12 18:33:56 +0200

    add finishTime and fix some bugs

---
+++ IQTest.DataAccess/Migrations/20210112154612_changeDateTimeInUserModelToTimeSpan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IQTest.DataAccess.Migrations
+{
+    public partial class changeDateTimeInUserModelToTimeSpan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Time",
+                table: "Users",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Time",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+        }
+    }
+}
